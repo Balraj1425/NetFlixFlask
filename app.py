0e837af0d9be815c3210560162115f70d6ce3909
Balraj <bhullar.bs@gmail.com>
2022-08-07 11:50:59 +0530

    WIP: basic addProgram API structure

--- app.py
+++ app.py
@@ -1,27 +1,28 @@
-from flask import Flask , request , jsonify
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 @app.route("/hello")
 def hello():
-    return "Hello World"
+    return "hello World"
 
-@app.route("/addPrograms", methods=["POST"])
-def addPrograms():
+@app.route("/addProgram", methods=["POST"])
+def addProgram():
     _json = request.json
-    name = _json["name"]    
+    name = _json["name"]
     programType = _json["programType"]
-    programId = _json(programId)
-    ImageSrc = _json("imageSrc")
-    trailerLink = _json("trailerLink")
-    videoLink = _json("videoLink")
-    uaRating = _json("uaRating")
-    description =_json("description")
-    castInfo = _json("castInfo")
-    creatorInfo = _json("creatorInfo")
-    yearOfMaking = _json("yearOfMaking")
-    genre = _json("genre")
-    seasonsNo = _json("seasonNo")
-    return jsonify("Added Successfully")
-    print("name,programType")
+    programId = _json["programId"]
+    imageSrc = _json["imageSrc"]
+    trailerLink = _json["trailerLink"]
+    videoLink = _json["videoLink"]
+    uaRating = _json["uaRating"]
+    description = _json["description"]
+    castInfo = _json["castInfo"]
+    creatorInfo = _json["creatorInfo"]
+    yearOfMaking = _json["yearOfMaking"]
+    genre = _json["genre"]
+    noOfSeasons = _json["noOfSeasons"]
+
+    print(name, programType, imageSrc,castInfo)
+    return jsonify("added succesfully")
 
 if __name__ == "__main__":
-        app.run(port = 3000)
+    app.run(port = 3000)
