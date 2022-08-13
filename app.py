a7a66bd87d1158daa61bdeb303869d0701a7ef2c
Sachin S <mrluckmaker@gmail.com>
2022-08-13 10:47:21 +0530

    init commit Sachin

---
+++ app.py
@@ -0,0 +1,63 @@
+from bson.json_util import dumps
+from flask import Flask, request, jsonify
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+app.secret_key = "secret"
+app.config["MONGO_URI"] = "mongodb+srv://admin:admin123@cluster0.gqmw0.mongodb.net/CRS?retryWrites=true&w=majority"
+mongo = PyMongo(app)
+
+@app.route("/hello")
+def hello():
+    return jsonify("hello World")
+
+@app.route("/addProgram", methods=["POST"])
+def addProgram():
+    _json = request.json
+    name = _json["name"]
+    programType = _json["programType"]
+    programId =int(_json["programId"]) 
+    imageSrc = _json["imageSrc"]
+    trailerLink = _json["trailerLink"]
+    videoLink = _json["videoLink"]
+    uaRating = _json["uaRating"]
+    description = _json["description"]
+    castInfo = _json["castInfo"]
+    creatorInfo = _json["creatorInfo"]
+    yearOfMaking = _json["yearOfMaking"]
+    genre = _json["genre"]
+    noOfSeasons = _json["noOfSeasons"]
+
+    dataInsert = mongo.db.netflixlist.insert_one({
+        "name": name, 
+        "programType":programType,
+        "programId":programId,
+        "imageSrc":imageSrc,
+        "trailerLink":trailerLink,
+        "videoLink":videoLink,
+        "uaRating":uaRating,
+        "description":description,
+        "castInfo":castInfo,
+        "creatorInfo":creatorInfo,
+        "yearOfMaking":yearOfMaking, 
+        "genre":genre,
+        "noOfSeasons":noOfSeasons
+    })
+
+    print(name, programType, imageSrc,castInfo)
+    return jsonify("added succesfully")
+
+@app.route("/getMovielist",methods=["GET"])
+def allDetails():
+    allDetail=mongo.db.netflixlist.find()
+    resp=dumps(allDetail)
+    return resp
+
+@app.route("/getMovieId/<int:id>",methods=["GET"])
+def getMovieId(id):
+    allDetail=mongo.db.netflixlist.find_one({"programId":id})
+    resp=dumps(allDetail)
+    return resp
+
+if __name__ == "__main__":
+    app.run(port = 3000,debug=True)
